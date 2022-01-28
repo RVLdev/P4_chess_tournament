99637332d44d3408d34fe2e6075f7fb8a21e2aaf
Rachel Volat <rachel1arvl@gmail.com>
2022-01-28 20:53:47 +0100

    calcul et update points joueurs end_round

--- p4_controllers.py
+++ p4_controllers.py
@@ -12,17 +12,19 @@ from tinydb import where, Query
 class TournamentController:
     def __init__(self):
         self.tournament_view = TournamentView()
-        self.tournament_players_list = []
+        self.tournament_players_id_list = []
         self.rounds_list = []
+        self.t_full_players_list = []
 
-    def create_new_tournament(self, tournament_id=0):
+    def create_new_tournament(self, tournament_rounds_list, tournament_id=0):
         """create one tournament"""
         tournament = Tournament(
             tournament_id,
             self.ask_tournament_name(),
             self.ask_tournament_place(),
             self.ask_tournament_date(),
-            self.create_tournament_players_list(),
+            tournament_rounds_list,  # rounds doc_ids list
+            self.create_tournament_players_id_list(),  # doc_ids (L65 & 68)
             self.ask_tournament_description(),
             self.ask_time_control(),
             self.ask_tournament_rounds_qty()
@@ -47,25 +49,26 @@ class TournamentController:
         tournament_date = input()
         return tournament_date
 
-    def create_tournament_players_list(self):
+    def create_tournament_players_id_list(self):  # players doc_ids
         """ create this tournament's list of 8 players"""
-        while len(self.tournament_players_list) < 8:
+        while len(self.tournament_players_id_list) < 8:
             self.add_player_to_tournament()
         print('liste des joueurs du tournoi complète')
-        return self.tournament_players_list
+        return self.tournament_players_id_list
 
     def add_player_to_tournament(self):
-        """ add player to tournament_players_list """
+        """ add player to tournament_players_id_list """
         # check if player in DB
         requested_player = PlayerController.request_player()
         # if player not in DB, launch its creation and add it to list
         if requested_player is None:
             newplayer = PlayerController.create_player()
-            self.tournament_players_list.append(newplayer)
+            Player.update_player_id()
+            self.tournament_players_id_list.append(newplayer.doc_id)
         # if player in DB, add it to list
         else:
-            self.tournament_players_list.append(requested_player)
-        return self.tournament_players_list
+            self.tournament_players_id_list.append(requested_player.doc_id)
+        return self.tournament_players_id_list
 
     def ask_tournament_description(self):
         """ get tournament description from User"""
@@ -85,36 +88,52 @@ class TournamentController:
         tournament_rounds_qty = input()
         return tournament_rounds_qty
 
-    def sort_tournament_players_list_by_rank(self):
+    def sort_tournament_players_id_list_by_rank(self):
         """sort by rank tournament players list"""
-        rank_sorted_p_list = sorted(self.tournament_players_list,
+        # récupère la liste complète des éléments à trier
+        for doc in self.tournament_players_id_list:
+            t_full_player = Player.players_db.get(doc_id=doc)
+            self.t_full_players_list.append(t_full_player)
+        
+        rank_sorted_p_list = sorted(self.t_full_players_list,
                                   key=lambda k: k['p_rank'])
-        print(rank_sorted_p_list)
+        print(rank_sorted_p_list)  # contient des joueurs 'complets' (pas liste de doc_ids)
         return rank_sorted_p_list
-
-    def sort_tournament_players_list_by_points(self, rank_sorted_p_list):
-        """ get tournament players list sorted by rank and total points """
-        # commencer par tri par rank puis trier par points => les "mêmes pts" seront déjà triés par rank !
-        points_sorted_p_list = sorted(rank_sorted_p_list,
-                                      key=lambda k: k['p_total_points'], reverse = True)
-        print(points_sorted_p_list)
-        return points_sorted_p_list
-
-
-    ##   A TRANSFERER DS TOURNOI - CALCUL A FAIRE et code à compléter/corriger
-    # update NB TOTAL POINTS DU JOUEUR ds players_db & tournament_players_list
+  
     def update_player_points_qty(self, player_id):
-        # first calculate then update p_total_points
-        # calcul ?
-        nbr_players = len(Tournament.tournament_players_list)
-        for j in range(0, nbr_players):
-            player_points = ' calcul???? '
-        # udate t_players_list
-        Tournament.tournament_players_list[j]['p_total_points'] = player_points
+        """ calculate and update players total points"""
+        nbr_players = len(Tournament.tournament_players_id_list)
+
+        for j in range(0, nbr_players/2):
+            match_pl1_doc_id = RoundController.r_matches_list[j]['chess_player1']
+            player1 = Player.players_db.get(doc_id=match_pl1_doc_id)  # ou doc_ids = []
+            previous_points_pl1 = player1['p_total_points']
+            
+            new_points_pl1 = RoundController.player1_score
+            new_total_points_pl1 = new_points_pl1 + previous_points_pl1
+            Player.players_db.update({'p_total_points': new_total_points_pl1},
+                                     doc_id=match_pl1_doc_id)  # ou doc_ids = []
+            
+            match_pl2_doc_id = RoundController.r_matches_list[j]['chess_player2']
+            player2 = Player.players_db.get(doc_id=match_pl2_doc_id)  # ou doc_ids = []
+            previous_points_pl2 = player2['p_total_points']
+            
+            new_points_pl2 = RoundController.player2_score
+            new_total_points_pl2 = new_points_pl2 + previous_points_pl2
+            Player.players_db.update({'p_total_points': new_total_points_pl2},
+                                     doc_id=match_pl2_doc_id)  # ou doc_ids = []
+        
 
         # update players_db
         Player.update_p_total_points(player_id, Player.player_points_qty)
 
+    def sort_tournament_players_id_list_by_points(self, rank_sorted_p_list):
+        """ get tournament players list sorted by rank and total points """
+        # commencer par tri par rank puis trier par points => les "mêmes pts" seront déjà triés par rank !
+        points_sorted_p_list = sorted(rank_sorted_p_list,
+                                      key=lambda k: k['p_total_points'], reverse = True)
+        print(points_sorted_p_list)  # contient des joueurs 'complets' (pas liste de doc_ids)
+        return points_sorted_p_list
 
     def create_first_round_matches(self, rank_sorted_p_list):
         """ create matches for first round """
@@ -122,10 +141,14 @@ class TournamentController:
         for i in range(0, matches_qty):
             RoundController.r_matches_list.append(
                 Match(
-                        rank_sorted_p_list[i],
-                        rank_sorted_p_list[i+matches_qty]
+                        rank_sorted_p_list[i]['p_id'],  # doc_id du match_player1
+                        rank_sorted_p_list[i+matches_qty]['p_id']  # doc_id du match_player2
                      )
             )
+
+    def fill_rounds_list(self):  # à remplir à la création ou à la clôture du round ?
+        #  ...
+        self.rounds_list.append()
 
     def create_next_round_matches(self, points_sorted_p_list):
         """ create matches for round > 1 """
@@ -135,12 +158,12 @@ class TournamentController:
         for i in range(0, matches_qty):
             RoundController.r_matches_list.append(
                 Match(
-                        points_sorted_p_list[i],
-                        points_sorted_p_list[i+1] # i+1 = joueur suivant ds liste totalemt triée
+                        points_sorted_p_list[i]['p_id'],  # doc_id du match_player1
+                        points_sorted_p_list[i+1]['p_id']  # i+1 = joueur suivant ds liste totalemt triée
                      )
             )
 
-    # alimente la liste de matchs (initialement vide)
+    # alimente la liste de matchs (initialement vide) => contient des joueurs 'complets' (pas liste de doc_ids)
     def add_match_to_r_matches_list(self, rank_sorted_p_list):
         """add new match to round matches list"""
         if len(Round.rounds_db) >= 1:
@@ -158,7 +181,7 @@ class RoundController:
         """ create a round """
         round = Round(round_id,
                       self.give_round_name(),
-                      self.fill_r_matches_list(),
+                      self.fill_r_matches_list(), # contient des joueurs 'complets' (pas liste de doc_ids)
                       self.set_start_date_time(),
                       end_date_time)
         round.create_round()
@@ -237,11 +260,17 @@ class RoundController:
 
         # print "joueur 1 : PLAYER1-NAME"
         print('joueur 1 : ' + self.r_matches_list[i]['chess_player1']['p_name'])
-
+        
         MatchView.ask_score_player()
         player1_score = input('saisissez son score (0 ou 0.5 ou 1) : ')
+        
+        # dico qui associe player doc_id et new points 
+        pl_doc_id = self.r_matches_list[i]['chess_player1']
+        new_points_player1 = {'pl_doc_id' : pl_doc_id, 'newpoints': player1_score}
+        
         print('joueur 1 : ' + self.r_matches_list[i]['chess_player1']['p_name']
                             + ' score = ' + player1_score)
+        
         return player1_score
 
     def ask_player2_score(self, i):
@@ -260,23 +289,8 @@ class RoundController:
                             + ' score = ' + player2_score)
         return player2_score
 
-""" TRANSFERE DS TOURNOI 
-    # CALCUL A FAIRE et code à compléter/corriger
-    # update NB TOTAL POINTS DU JOUEUR ds players_db & tournament_players_list
-    def update_player_points_qty(self, player_id):
-        # first calculate then update p_total_points
-        # calcul ?
-        nbr_players = len(Tournament.tournament_players_list)
-        for j in range(0, nbr_players):
-            player_points = ' calcul???? '
-        # udate t_players_list
-        Tournament.tournament_players_list[j]['p_total_points'] = player_points
 
-        # update players_db
-        Player.update_p_total_points(player_id, Player.player_points_qty)
-"""
-
-class MatchController:
+class MatchController:  # INUTILE ? (pas utilisé ds code v28/01)
     def __init__(self):
         pass
 
@@ -291,30 +305,6 @@ class MatchController:
         match.create_match()
         Match.update_match_id()
         return match
-
-    """ TRANSFERE DANS ROUNDCONTROLLER
-    def update_match_score(self, match_id, player1_score, player2_score):
-        player1_score = self.ask_player1_score()
-        player2_score = self.ask_player2_score()
-        Match.update_players_scores(match_id, player1_score, player2_score)
-
-    def ask_player1_score(self, Round.r_matches_list):
-        # print "match PLAYER1-NAME / PLAYER2-NAME"
-        print('match ' + Round.r_matches_list[0]['chess_player1']['p_name']
-                       + " / "
-                       + Round.r_matches_list[0]['chess_player2']['p_name'])
-
-        # print "joueur 1 : PLAYER1-NAME"
-        print('joueur 1 : ' + Round.r_matches_list[0]['chess_player1']['p_name'])
-
-        MatchView.ask_score_player()
-        player1_score = input('saisissez son score (0 ou 0.5 ou 1) : ')
-        print('joueur 1 : '+ Round.r_matches_list[0]['chess_player1']['p_name']
-                           + ' score = ' + player1_score)
-        return player1_score
-
-    def ask_player2_score(self):
-        pass"""
 
 
 class PlayerController:
@@ -372,12 +362,15 @@ class PlayerController:
         search_p_name = self.ask_player_name()
         search_p_firstname = self.ask_player_first_name()
         Theplayer = Query()
-        requested_player = Player.players_db.search(
+        requested_player = Player.players_db.get(
             (
                 Theplayer.p_name == search_p_name
             ) & (
                 Theplayer.p_firstname == search_p_firstname
                 )
             )
-        print(requested_player)  # = list with a dictionnary within !
-        return requested_player
+        # Search donne [{}] (liste contenant joueur), 
+        # mais 'get' donne directement le joueur {} 
+        print(requested_player)
+        print(requested_player.doc_id)  # DOC_ID du joueur
+        return requested_player.doc_id
